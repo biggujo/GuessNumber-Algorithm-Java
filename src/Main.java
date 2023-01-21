1d93e1d3d204c4dabee20291d92865e9d46575a7
biggujo <gen.style22@gmail.com>
2023-01-21 12:49:11 +0200

    fix unfinite guessing

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,27 @@ public class Main {
         int lowRange = 1;
         int highRange = 100;
         int possibleGuesses;
-        int yourGuess;
+        int yourGuess = 0;
         int playerAnswer = 0;
         int countNumTurns = 1;
+
+        int prevValue = 0;
 
         while (playerAnswer != 1) {
             possibleGuesses = highRange + lowRange - 1;
             yourGuess = (int) Math.ceil(possibleGuesses / 2);
 
+            // If no changes since previous turn or
+            // if there are already 7 turns (maximum)
+            if (prevValue == yourGuess || countNumTurns == 7) {
+
+                // Stop guessing, result is defined
+                break;
+            }
+
             System.out.printf("Is your age %d ?\n", yourGuess);
             System.out.printf("1 - Yes\n2 - Guess a lower age\n3 - Guess a higher age\nAction: ");
             playerAnswer = input.nextInt();
-
 
             if(playerAnswer == 3)
                 lowRange = yourGuess + 1;
@@ -31,9 +40,13 @@ public class Main {
                 break;
 
             countNumTurns++;
+
+            // Remember previous guess
+            prevValue = yourGuess;
         }
 
         System.out.printf("I guessed your age in %d turns !\n", countNumTurns);
+        System.out.printf("Age: %d\n", yourGuess);
 
     }
 }
